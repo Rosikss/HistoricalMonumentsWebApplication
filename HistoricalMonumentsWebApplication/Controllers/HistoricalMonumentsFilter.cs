53c64287365643d6dab8c99ab670989039e11c01
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2024-03-05 21:32:25 +0200

    1.3 filter

--- HistoricalMonumentsWebApplication/Controllers/HistoricalMonumentsFilter.cs
+++ HistoricalMonumentsWebApplication/Controllers/HistoricalMonumentsFilter.cs
@@ -1,47 +1,68 @@
 ﻿using HistoricalMonumentsWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Linq;
 
-namespace HistoricalMonumentsWebApplication.Controllers
+namespace HistoricalMonumentsWebApplication.Controllers;
+
+public class HistoricalMonumentsFilter : Controller
 {
-    public class HistoricalMonumentsFilter : Controller
+    private const int PageItems = 8;
+    private readonly DblibraryContext _context;
+
+    public HistoricalMonumentsFilter(DblibraryContext context)
     {
-        private readonly DblibraryContext _context;
-        private const int PageItems = 6;
+        _context = context;
+    }
 
-        public HistoricalMonumentsFilter(DblibraryContext context)
+    [Route("historical-monuments/{category?}")]
+    public async Task<IActionResult> Index([FromQuery] string categoryOption, [FromRoute] string? category = "all",
+        [FromQuery] int page = 1)
+    {
+        ViewBag.Cities = await _context.Cities.Select(city => city.Name).Distinct().ToListAsync();
+        ViewBag.Classifications = await _context.Classifications.Select(classification => classification.Name).Distinct().ToListAsync();
+
+        Dictionary<string, Func<HistoricalMonument, bool>> _filters = new()
         {
-            _context = context;
-        }
-        [Route("historical-monuments")]
-        public async Task<IActionResult> Index([FromQuery] int page = 1)
+            ["classification"] = historicalMonument => historicalMonument.Classification.Name.ToLower() == categoryOption.ToLower(),
+            ["city"] = historicalMonument => historicalMonument.City.Name.ToLower() == categoryOption.ToLower()
+        };
+
+
+        var allMonuments = await _context.HistoricalMonuments.Include(h => h.City)
+            .Include(h => h.Classification)
+            .Include(h => h.Status).ToListAsync();
+
+        var totalElements = allMonuments.Count;
+
+        var pages = (int)Math.Ceiling((decimal)totalElements / PageItems);
+
+
+        if (page > pages || page < 1 || string.IsNullOrEmpty(category)) return RedirectToAction(nameof(Index), new { page = 1, category = "all" });
+
+        List<HistoricalMonument> filteredMonuments = category == "all" ? allMonuments : Filter(allMonuments, _filters[category]);
+
+        var paginationHistoricalMonuments = filteredMonuments.Skip((page - 1) * PageItems).Take(PageItems).ToList();
+
+        totalElements = filteredMonuments.Count;
+
+        pages = (int)Math.Ceiling((decimal)totalElements / PageItems);
+
+        var paginationModel = new PaginationModel
         {
+            HistoricalMonuments = paginationHistoricalMonuments,
+            Pages = pages,
+            CurrentPage = page,
+            Category = category,
+            CategoryOption = string.IsNullOrEmpty(categoryOption)  ? "Empty" : categoryOption
+        };
 
-            
+        return View(paginationModel);
+    }
 
-            int totalElements = await _context.HistoricalMonuments.CountAsync();
-
-            int pages = (int)Math.Ceiling((decimal)totalElements / PageItems);
-            
-            if (page > pages || page < 1)
-            {
-                return RedirectToAction(nameof(Index), new { page = 1 });
-            }
-
-            var countries = await _context.Countries.ToListAsync();
-
-            ViewBag.Countries = countries;
-
-            var paginationHistoricalMonuments = await _context.HistoricalMonuments.Skip((int)((page - 1) * PageItems)).Take(PageItems).ToListAsync();
-            
-            PaginationModel paginationModel2 = new PaginationModel
-            {
-                HistoricalMonuments = paginationHistoricalMonuments,
-                Pages = pages,
-                
-            };
-            return View(paginationModel2);
-
-        }
+    private List<HistoricalMonument> Filter(List<HistoricalMonument> allMonuments, Func<HistoricalMonument, bool> predicate)
+    {
+        return allMonuments.Where(predicate).ToList();
     }
 }
--- HistoricalMonumentsWebApplication/Models/PaginationModel.cs
+++ HistoricalMonumentsWebApplication/Models/PaginationModel.cs
@@ -6,5 +6,9 @@
 
         public int? Pages { get; set; }
 
+        public int? CurrentPage { get; set;}
+
+        public string Category { get; set; }
+        public string? CategoryOption { get; set; }
     }
 }
